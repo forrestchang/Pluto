08a2965ace27787664fd578f5ddcf3516263121b
Jiayuan Zhang <forrestchang7@gmail.com>
2015-12-12 23:02:42 +0800

    pluto now is a simple math calculator

--- Pluto/pluto.py
+++ Pluto/pluto.py
@@ -1,6 +1,9 @@
-__author__ = 'forrest'
-
-INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+INTEGER = 'INTEGER'
+PLUS = 'PLUS'
+MINUS = 'MINUS'
+MULTIPLY = 'MULTIPLY'
+DIVIDE = 'DIVIDE'
+EOF = 'EOF'
 
 class Token(object):
     def __init__(self, type, value):
@@ -8,7 +11,7 @@ class Token(object):
         self.value = value
 
     def __str__(self):
-        return 'Token({type}, {value})'.format(type = self.type, value = repr(self.value))
+        return '({type}, {value})'.format(type = self.type, value = self.value)
 
     def __repr__(self):
         return self.__str__()
@@ -18,54 +21,110 @@ class Interpreter(object):
         self.text = text
         self.pos = 0
         self.current_token = None
+        self.current_char = self.text[self.pos]
 
     def error(self):
         raise Exception('Error parsing input')
 
-    def get_next_token(self):
-        text = self.text
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
-        current_char = text[self.pos]
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
-            return token
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
-        self.error()
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) -1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
 
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
+    def skip_whitespace(self):
+        if self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTIPLY, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIVIDE, '/')
+
+            self.error()
+        return Token(EOF, None)
+
+    def eat(self, type):
+        if self.current_token.type == type:
             self.current_token = self.get_next_token()
         else:
             self.error()
 
     def expr(self):
         self.current_token = self.get_next_token()
+
         left = self.current_token
         self.eat(INTEGER)
+
         op = self.current_token
-        self.eat(PLUS)
+        if op.type == PLUS:
+            self.eat(PLUS)
+        elif op.type == MINUS:
+            self.eat(MINUS)
+        elif op.type == MULTIPLY:
+            self.eat(MULTIPLY)
+        else:
+            self.eat(DIVIDE)
+
         right = self.current_token
         self.eat(INTEGER)
 
-        result = left.value + right.value
+        if op.type == PLUS:
+            result = left.value + right.value
+        elif op.type == MINUS:
+            result = left.value - right.value
+        elif op.type == MULTIPLY:
+            result = left.value * right.value
+        else:
+            if right.value == 0:
+                raise Exception('Math error')
+            else:
+                result = left.value / right.value
+
         return result
 
 def main():
     while True:
         try:
-            text = input('calc> ')
+            text = input('>>> ')
         except EOFError:
             break
         if not text:
             continue
+
         interpreter = Interpreter(text)
         result = interpreter.expr()
         print(result)
 
 if __name__ == '__main__':
     main()
+
