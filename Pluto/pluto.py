5fe6e004e9f2079ede153cb4e5064cf2ef19c180
Jiayuan Zhang <forrestchang7@gmail.com>
2015-12-22 21:32:03 +0800

    update some code

--- Pluto/pluto.py
+++ Pluto/pluto.py
@@ -1,11 +1,7 @@
-__author__ = 'forrest'
-
 INTEGER = 'INTEGER'
-EOF = 'EOF'
 PLUS = 'PLUS'
 MINUS = 'MINUS'
-MULTIPLY = 'MULTIPLY'
-DIVIDE = 'DIVIDE'
+EOF = 'EOF'
 
 class Token(object):
     def __init__(self, type, value):
@@ -33,73 +29,29 @@ class Interpreter(object):
 
         if self.pos > len(self.text) - 1:
             self.current_char = None
-        else:
-            self.current_char = self.text[self.pos]
+
+        self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
-
-    def integer(self):
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
 
     def get_next_token(self):
         while self.current_char is not None:
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
-
             if self.current_char.isdigit():
-                return Token(INTEGER, self.integer())
-
+                self.advance()
             if self.current_char == '+':
                 self.advance()
                 return Token(PLUS, '+')
-
             if self.current_char == '-':
                 self.advance()
                 return Token(MINUS, '-')
-
-            if self.current_char == '*':
-                self.advance()
-                return Token(MULTIPLY, '*')
-
-            if self.current_char == '/':
-                self.advance()
-                return Token(DIVIDE, '/')
-
             self.error()
-
         return Token(EOF, None)
 
-    def eat(self, type):
-        if self.current_token.type == type:
-            self.current_token = self.get_next_token()
-        else:
-            self.error()
-
-    def term(self):
-        token = self.current_token
-        self.eat(INTEGER)
-        return token.value
-
-    def expr(self):
-        self.current_token = self.get_next_token()
-
-        result = self.term()
-        while self.current_token.type in (PLUS, MINUS):
-            token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result = result - self.term()
-        return result
 
 def main():
     while True:
